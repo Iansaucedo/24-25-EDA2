3b1f1c322009114ec452e714dc6cd4c3af7bc3dc
ian saucedo <iamasterpro1@gmail.com>
2025-05-19 10:24:10 +0200

    Hasher

--- entregas/SaucedoIan/StudentHasher.java
+++ entregas/SaucedoIan/StudentHasher.java
@@ -3,21 +3,18 @@ public class StudentHasher {
     private static final int P = 31;
     private static final int M = 1_000_000_007;
 
-  public static long polynomialHash(String input) {
-      long hash = 0;
-      long power = 1;
+    public static long polynomialHash(String input) {
+        long hash = 0;
+        long power = 1;
 
-      for (int i =public class HashingStudents {
-  
-      }
-       0; i < input.length(); i++) {
-          char c = input.charAt(i);
-          hash = (hash + (c * power) % M) % M;
-          power = (power * P) % M;
-      }
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            hash = (hash + (c * power) % M) % M;
+            power = (power * P) % M;
+        }
 
-      return hash;
-  }
+        return hash;
+    }
 
     public static long hashStudent(String fullName, String carnet, String courseCode, double grade) {
         String normalized = (fullName.trim() + "|" + carnet.trim() + "|" + courseCode.trim() + "|" + grade)
